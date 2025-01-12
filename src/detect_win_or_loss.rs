e26c7c0d808a22d1ba8d27439c13fb9f3147b4c4
Eric Wolf <1983821+typetetris@users.noreply.github.com>
2025-01-12 14:57:25 +0100

    Delay end screens abortability

--- src/detect_win_or_loss.rs
+++ src/detect_win_or_loss.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 
 use crate::{
@@ -13,8 +15,27 @@ pub struct DetectWinOrLossPlugin;
 #[derive(Component)]
 struct OnEndScreen;
 
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, SubStates)]
+#[source(GameStates = GameStates::End)]
+enum EndPhase {
+    #[default]
+    MinimumDisplayTime,
+    Abortable,
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, SubStates)]
+#[source(EndPhase = EndPhase::MinimumDisplayTime)]
+enum Steps {
+    #[default]
+    One,
+    Two,
+    Three,
+}
+
 impl Plugin for DetectWinOrLossPlugin {
     fn build(&self, app: &mut App) {
+        app.add_sub_state::<EndPhase>();
+        app.add_sub_state::<Steps>();
         app.add_systems(
             Update,
             detect_win_or_loss.run_if(in_state(GameStates::Game)),
@@ -22,7 +43,11 @@ impl Plugin for DetectWinOrLossPlugin {
         app.add_systems(OnEnter(GameStates::End), setup_end_screen);
         app.add_systems(
             Update,
-            start_game_on_button_press.run_if(in_state(GameStates::End)),
+            start_game_on_button_press.run_if(in_state(EndPhase::Abortable)),
+        );
+        app.add_systems(
+            Update,
+            update_end_screen.run_if(in_state(EndPhase::MinimumDisplayTime)),
         );
         app.add_systems(
             OnExit(GameStates::End),
@@ -59,6 +84,12 @@ fn detect_win_or_loss(
         }
     }
 }
+
+#[derive(Resource)]
+struct EndScreenMinimumDisplayTimer(Timer);
+
+#[derive(Component)]
+struct UpdateableText;
 
 fn setup_end_screen(mut cmd: Commands, assets: Res<load_assets::Assets>, end_state: Res<EndState>) {
     let text = Text::new(if end_state.player_did_win {
@@ -100,7 +131,8 @@ fn setup_end_screen(mut cmd: Commands, assets: Res<load_assets::Assets>, end_sta
             },
         ));
         p.spawn((
-            Text::new("Drücke den Schießenknopf für einen weiteren Versuch!"),
+            UpdateableText,
+            Text::new("."),
             TextFont {
                 font: assets.orbitron_font.clone(),
                 font_size: 32.0,
@@ -114,4 +146,43 @@ fn setup_end_screen(mut cmd: Commands, assets: Res<load_assets::Assets>, end_sta
         ));
         p.spawn(AudioPlayer::new(sound));
     });
+    cmd.insert_resource(EndScreenMinimumDisplayTimer(Timer::new(
+        Duration::from_secs(1),
+        TimerMode::Repeating,
+    )));
+}
+
+#[allow(clippy::too_many_arguments)]
+fn update_end_screen(
+    time: Res<Time>,
+    mut end_screen_minimum_display_timer: ResMut<EndScreenMinimumDisplayTimer>,
+    mut next_state: ResMut<NextState<EndPhase>>,
+    step: Res<State<Steps>>,
+    mut next_step_state: ResMut<NextState<Steps>>,
+    updateable_text: Single<Entity, With<UpdateableText>>,
+    mut text_writer: TextUiWriter,
+    mut cmd: Commands,
+) {
+    if end_screen_minimum_display_timer
+        .0
+        .tick(time.delta())
+        .just_finished()
+    {
+        match step.get() {
+            Steps::One => {
+                next_step_state.set(Steps::Two);
+                *text_writer.text(*updateable_text, 0) = "..".to_string();
+            }
+            Steps::Two => {
+                next_step_state.set(Steps::Three);
+                *text_writer.text(*updateable_text, 0) = "...".to_string();
+            }
+            Steps::Three => {
+                next_state.set(EndPhase::Abortable);
+                *text_writer.text(*updateable_text, 0) =
+                    "Drücke den Schießenknopf, um es nochmal zu probieren!".to_string();
+                cmd.remove_resource::<EndScreenMinimumDisplayTimer>();
+            }
+        }
+    }
 }
