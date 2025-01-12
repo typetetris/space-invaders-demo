d8a11e6f88ef7bcc4e8b4499c11c8fe3e8430f30
Eric Wolf <1983821+typetetris@users.noreply.github.com>
2025-01-12 14:57:13 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in with pkgs; {
+    devShells.x86_64-linux.default = mkShell rec {
+      nativeBuildInputs = [
+        pkg-config
+      ];
+      buildInputs =[
+        udev
+        alsa-lib
+        vulkan-loader
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        libxkbcommon
+        wayland 
+      ];
+      LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+    };
+  };
+}
